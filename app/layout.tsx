0fb6f00137344e47dfd7f00e6094ffdf8205d09c
Paul Nogaro <paulnogaro@MacBook-Air-de-Paul.local>
2026-01-11 10:49:15 +0100

    feat: Ajout Google Tag Manager (GTM-57WMHVFC) pour suivi analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins, Playfair_Display } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { ProximityProvider } from '@/components/ProximityProvider'
 
@@ -68,7 +69,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-57WMHVFC');
+          `,
+        }}
+      />
       <body className={`${poppins.variable} ${playfair.variable} font-sans antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-57WMHVFC"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <ProximityProvider>
           {children}
         </ProximityProvider>
